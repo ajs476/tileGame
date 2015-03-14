51fb2de42227d4d018d74f528af9a70cf80db453
Hayden <haydentwestbrook@gmail.com>
2015-03-13 20:45:58 -0700

    Added basic camera functions

--- src/Game.hx
+++ src/Game.hx
@@ -6,20 +6,45 @@ import starling.events.Event;
 import starling.core.Starling;
 import starling.display.Image;
 import starling.display.DisplayObject;
+import starling.events.KeyboardEvent;
+import starling.animation.Tween;
 import Tilemap;
 
 class Game extends Sprite {
 
 	var tileMap:Tilemap;
-	var camera = 
 
 	public function new() {
 		super();
 		createMap();
+		Starling.current.stage.addEventListener(KeyboardEvent.KEY_DOWN, moveCamera);
 	}
 
 	public function createMap() {
 		tileMap = new Tilemap(Root.assets, "map");
 		addChild(tileMap);
+	}
+
+	public function moveCamera(event:KeyboardEvent) {
+		if(event.keyCode == 68) {
+			if(this.x - 64 <= 0) {
+				this.x -= 64;
+			}
+		}
+		if(event.keyCode == 87) {
+			if(this.y + 64 <= 0) {
+				this.y += 64;
+			}
+		}
+		if(event.keyCode == 65) {
+			if(this.x + 64 <= 0) {
+				this.x += 64;
+			}
+		}
+		if(event.keyCode == 83) {
+			if(this.y - 64 <= 0) {
+				this.y -= 64;
+			}
+		}
 	}
 }
